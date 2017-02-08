15de75273645326d6c07adb68059f94bc1d2d6a4
Drew DeVault <sir@cmpwn.com>
2017-02-08 06:43:47 -0500

    Add setup.py, move stuff into srht/

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+import subprocess
+import glob
+setup(
+  name = 'srht',
+  packages = ['srht'],
+  version = subprocess.run(['git', 'describe', '--tags'],
+      stdout=subprocess.PIPE).stdout.decode().strip(),
+  description = 'sr.ht core modules',
+  author = 'Drew DeVault',
+  author_email = 'sir@cmpwn.com',
+  url = 'https://git.sr.ht/~sircmpwn/srht',
+  requires = ['flask', 'humanize', 'sqlalchemy'],
+  license = 'GPL-2.0',
+  package_data={
+      'srht': [
+          'Makefile',
+          'minify-css.js',
+          'package.json',
+          'templates/*.html',
+          'scss/*.scss',
+          'scss/bootstrap/LICENSE',
+      ] + [f[5:] for f in glob.glob('srht/node_modules/**', recursive=True)] \
+        + [f[5:] for f in glob.glob('srht/scss/bootstrap/scss/**/*.scss', recursive=True)]
+  }
+)
