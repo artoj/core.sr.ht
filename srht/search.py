5936d2eaaa9bc66e1ab167613bc306cb1a2b169b
Drew DeVault <sir@cmpwn.com>
2019-02-05 09:45:47 -0500

    Add search helper module

---
+++ srht/search.py
@@ -0,0 +1,30 @@
+import shlex
+from sqlalchemy import or_
+
+def search(query, terms, default_props, prop_map):
+    # TODO: OR, case-sensitivity(?)
+    terms = shlex.split(terms)
+    for term in terms:
+        term = term.lower()
+        if ":" in term:
+            parts = term.split(":")
+            prop = parts[0]
+            value = ":".join(parts[1:])
+        else:
+            prop, value = None, term
+        if prop is None:
+            query = query.filter(or_(*[
+                    p.ilike("%" + value + "%")
+                for p in default_props]))
+            continue
+        if prop in prop_map:
+            prop = prop_map[prop]
+            if callable(prop):
+                query = prop(query, value)
+            else:
+                query = query.filter(prop.ilike("%" + value + "%"))
+        elif None in prop_map:
+            query = prop_map[None](query, prop, value)
+        else:
+            query = query.filter(False)
+    return query
