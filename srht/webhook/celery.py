8e7921d11f40b88c47d29a66eb603db01e2ee3e7
Drew DeVault <sir@cmpwn.com>
2019-04-04 11:03:40 -0400

    Let application choose Celery broker for webhooks

--- srht/webhook/celery.py
+++ srht/webhook/celery.py
@@ -7,39 +7,42 @@ from srht.database import db
 from srht.webhook import Webhook
 import requests
 
-worker = Celery('webhooks', broker='redis://')
+def make_worker(broker='redis://'):
+    worker = Celery('webhooks', broker=broker)
 
-@worker.task
-def async_request(url, payload, headers, delivery_table=None, delivery_id=None):
-    """
-    Performs an HTTP POST asyncronously, and updates the delivery row if a
-    table & id is specified.
-    """
-    try:
-        r = requests.post(url, data=payload, timeout=5, headers=headers)
-        response = r.text
-        response_status = r.status_code
-        response_headers = "\n".join(
-                f"{key}: {value}" for key, value in r.headers.items())
-    except requests.exceptions.ReadTimeout:
-        response = "Request timeed out after 5 seconds."
-        response_status = -1
-        response_headers = None
-    if delivery_table and delivery_id:
-        db.session.execute(
-            f"""
-            UPDATE {delivery_table}
-            SET response = :response,
-                response_status = :status,
-                response_headers = :headers
-            WHERE id = :delivery_id
-            """, {
-                "response": response,
-                "status": response_status,
-                "headers": response_headers,
-                "delivery_id": delivery_id
-            })
-        db.session.commit()
+    @worker.task
+    def async_request(url, payload, headers, delivery_table=None, delivery_id=None):
+        """
+        Performs an HTTP POST asyncronously, and updates the delivery row if a
+        table & id is specified.
+        """
+        try:
+            r = requests.post(url, data=payload, timeout=5, headers=headers)
+            response = r.text
+            response_status = r.status_code
+            response_headers = "\n".join(
+                    f"{key}: {value}" for key, value in r.headers.items())
+        except requests.exceptions.ReadTimeout:
+            response = "Request timeed out after 5 seconds."
+            response_status = -1
+            response_headers = None
+        if delivery_table and delivery_id:
+            db.session.execute(
+                f"""
+                UPDATE {delivery_table}
+                SET response = :response,
+                    response_status = :status,
+                    response_headers = :headers
+                WHERE id = :delivery_id
+                """, {
+                    "response": response,
+                    "status": response_status,
+                    "headers": response_headers,
+                    "delivery_id": delivery_id
+                })
+            db.session.commit()
+
+    return worker
 
 class CeleryWebhook(Webhook):
     def process_delivery(cls, delivery, headers):
