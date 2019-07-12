51edd879a7d68ce788c6d6a17dbcd722b6aee31f
Drew DeVault <sir@cmpwn.com>
2019-07-12 14:22:18 -0400

    Don't use strong session protection (breaks SSO)

--- srht/flask/flask.py
+++ srht/flask/flask.py
@@ -121,7 +121,6 @@ class SrhtFlask(Flask):
         self.login_manager = LoginManager()
         self.login_manager.init_app(self)
         self.login_manager.anonymous_user = lambda: None
-        self.login_manager.session_protection = "strong"
 
         if self.oauth_service and self.oauth_service.User:
             @self.login_manager.user_loader
