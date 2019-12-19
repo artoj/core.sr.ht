0121485c1afcc877e0b412cdd1ed7138988feb4a
Drew DeVault <sir@cmpwn.com>
2019-12-19 10:09:45 -0500

    Add support for fully qualified redis URLs

--- srht/redis.py
+++ srht/redis.py
@@ -1,4 +1,11 @@
 from redis import Redis
 from srht.config import cfg
+from urllib.parse import urlparse
 
-redis = Redis(host=cfg("sr.ht", "redis-host", "localhost"))
+url = cfg("sr.ht", "redis-host", "localhost")
+url = urlparse(url)
+
+redis = Redis(host=url.hostname,
+        port=(url.port or 6379),
+        password=url.password,
+        db=int(url.path[1:]) if url.path else 0)
