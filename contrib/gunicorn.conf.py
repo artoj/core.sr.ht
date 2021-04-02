7d6920453d44c0e9ffa51155cb37d1e18e15267f
Ignas Kiela <me@ignaskiela.eu>
2021-04-02 12:59:44 +0300

    Create the metrics directory in gunicorn config

--- contrib/gunicorn.conf.py
+++ contrib/gunicorn.conf.py
@@ -5,6 +5,7 @@ import os
 def on_starting(server):
     multiprocess_dir = os.environ["prometheus_multiproc_dir"]
     shutil.rmtree(multiprocess_dir)
+    os.mkdir(multiprocess_dir)
 
 def child_exit(server, worker):
     multiprocess.mark_process_dead(worker.pid)
